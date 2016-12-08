83e349242b5aa53e06f1ccf0280ee3e7728984ae
Elors <ElorsAt@iCloud.com>
2016-12-08 14:59:57 +0800

    Add toJSON method for Book model

--- server/Bilibili_spy/models.py
+++ server/Bilibili_spy/models.py
@@ -6,4 +6,10 @@ class Book(models.Model):
     an_id = models.CharField(max_length=50)
 
     def __str__(self):
-        return '%s: %s' %(self.qs_id, self.an_id)
+        return '%s: %s' % (self.qs_id, self.an_id)
+
+    def toJSON(self):
+        return {
+            'id': self.qs_id,
+            'hash': self.an_id,
+        }
