b03646f806e20633db939040468c82e6134c1c2b
Elors <ElorsAt@iCloud.com>
2016-12-08 11:36:28 +0800

    Add upload url

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from Bilibili_spy import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^exam/upload/$', views.ExamView.as_view(), name="exam_view"),
 ]
