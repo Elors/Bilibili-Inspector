f2c33c38af71f1e8e03d806c5d12bff4a3b88d4b
Elors <ElorsAt@iCloud.com>
2016-12-08 18:31:04 +0800

    Modify inspector.js

--- extensions/src/inspector.js
+++ extensions/src/inspector.js
@@ -5,29 +5,30 @@ class Inspector {
     }
 
     innerToolBar() {
-        console.debug('inner ToolBar')
+        console.debug('inner ToolBar');
 
+        let object = this;
         let toolBar = document.createElement('div');
         toolBar.id = 'toolBar';
         toolBar.setAttribute('style', 'top: 415px; left: 0%; width: 150px; height: 260px; position: fixed; margin-left: 30px; background-color: #F25D8E; opacity: 0.55; border-radius: 5px;');
 
         let btn1 = document.createElement('div');
-        btn1.id = 'btn1';
         btn1.setAttribute('style', 'margin: 10px 15px; width: 120px; height: 110px; float: left; background-color: #FFF; border-radius: 5px; text-align: center; vertical-align: middle;');
         let span1 = document.createElement('span');
         span1.setAttribute('style', 'color: #F25D8E; line-height: 110px; ');
         span1.innerHTML = '自动答题';
         btn1.appendChild(span1);
-        btn1.addEventListener('click', Inspector.load);
+        btn1.addEventListener('click', function() {
+            object.load(object);
+        });
 
         let btn2 = document.createElement('div');
-        btn2.id = 'btn2';
         btn2.setAttribute('style', 'margin: 10px 15px; width: 120px; height: 110px; float: left; background-color: #FFF; border-radius: 5px; text-align: center; vertical-align: middle;');
         let span2 = document.createElement('span');
         span2.setAttribute('style', 'color: #F25D8E; line-height: 110px; ');
         span2.innerHTML = '上传答案';
         btn2.appendChild(span2);
-        let object = this;
+
         btn2.addEventListener('click', function() {
             object.upload(object);
         });
@@ -37,49 +38,53 @@ class Inspector {
         document.body.getElementsByClassName('answerRight')[0].appendChild(toolBar)
     }
 
-    static load() {
+    load(object) {
         let xmlhttp = new XMLHttpRequest();
-        xmlhttp.open('GET', 'base', true);
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.info("YES!");
-            console.info(xmlhttp.responseText);
-            this.run()
-        }
+        xmlhttp.open('GET', 'http://127.0.0.1:8000/exam/load/', true);
+
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4) {
+                object.books = JSON.parse(xmlhttp.responseText);
+                console.debug('已加载'+object.books.length+'条答案');
+                object.run()
+            }
+        };
         xmlhttp.send()
     }
 
-    run() {
+    run(object) {
         let exams = document.getElementById('examListUl').children;
         for (let exam of exams) {
             let qid = exam.getAttribute('qs_id');
-            let key = this.books[qid];
+            let key = object.books[qid];
             let list = exam.getElementsByClassName('solution')[0].children[0].children;
 
+            let count = 0
             for (let node of list) {
                 let hash = node.getAttribute('hash');
                 if (key === hash) {
                     node.click();
-                    console.info('MATCH: ' + qid + ' - ' + hash)
+                    // console.info('MATCH: ' + qid + ' - ' + hash)
+                    count += 1
                 }
             }
+            console.debug('共匹配'+count+'条答案')
         }
     }
 
-    hookAnswer() {
-        console.debug('hook answer')
+    hookAnswer(e) {
+        console.debug('hook answer');
         let exams = document.getElementById('examListUl').children;
         let object = this;
         for (let exam of exams) {
             let list = exam.getElementsByClassName('solution')[0].children[0].children;
 
             for (let node of list) node.addEventListener('click', function () {
-                let book = {
-                    "id": this.parentNode.parentNode.parentNode.getAttribute('qs_id'),
-                    "hash": this.getAttribute('hash')
-                };
-                object.books.push(book);
+                let id = this.parentNode.parentNode.parentNode.getAttribute('qs_id');
+                let hash = this.getAttribute('hash');
+                object.books[id] = hash
 
-                console.info('MATCH: ' + book.id + ' - ' + book.hash)
+                console.info('MATCH: ' + id + ' - ' + hash)
             });
         }
     }
@@ -92,8 +97,10 @@ class Inspector {
         let xmlhttp = new XMLHttpRequest();
         xmlhttp.open('POST', url, true);
         xmlhttp.onreadystatechange = function() {
-            console.debug(xmlhttp.responseText);
-        }
+            if (xmlhttp.readyState == 4) {
+                console.debug(xmlhttp.responseText);
+            }
+        };
         xmlhttp.send(null)
     }
 }
