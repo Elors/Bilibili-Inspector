8e8764afa470fe2ee3bd17ab706f8e87434602dc
Elors <ElorsAt@iCloud.com>
2016-12-08 11:36:08 +0800

    Display question detail

--- server/Bilibili_spy/models.py
+++ server/Bilibili_spy/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Book(models.Model):
-    '''
-
-    '''
     qs_id = models.CharField(max_length=50)
     an_id = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '%s: %s' %(self.qs_id, self.an_id)
